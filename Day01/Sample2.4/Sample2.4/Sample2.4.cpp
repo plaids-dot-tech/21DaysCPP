98d0390d9e6515784883863d72cae901e3bd43fc
plaids-dot-tech <jerrychi05@qq.com>
2020-11-08 22:45:20 +0800

    Day01

---
+++ Day01/Sample2.4/Sample2.4/Sample2.4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int DemoConsoleOutput();
+
+int main()
+{
+	DemoConsoleOutput();
+
+	return 0;
+}
+
+int DemoConsoleOutput()
+{
+	cout<<"This is a simple string literal"<<endl;
+	cout<<"Writing number five: "<< 5 <<endl;
+	cout<<"Performing devision 10 / 5 = "<<10/5<<endl;
+	cout<<"Pi when approximated is 22 / 7 = "<<22/7<<endl;
+	cout<<"Pi more accurately is 22 / 7 = "<<22.0/7<<endl;
+
+	return 0;
+}
